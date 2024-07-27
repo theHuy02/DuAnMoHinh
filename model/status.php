406dd0973c70aade1f239e9203fe53b665ddf776
NguyenVanThien696 <thanthien01102004@gmail.com>
2024-07-27 15:29:58 +0700

    status

--- model/status.php
+++ model/status.php
@@ -8,6 +8,7 @@
     function update($newStatus,$id_order) {
         $sql = "UPDATE tbl_order SET status = '$newtrangthai' WHERE id = $id_order";
         pdo_execute($sql);
+        // var_dump($sql);
     }
 
     echo $status;
