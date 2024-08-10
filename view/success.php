13cd930c0b9d15ad606a9ceb8a99c6abecd4ec68
theHuy02 <lethehuy1234567@gmail.com>
2024-08-10 19:32:09 +0700

    success update

--- view/success.php
+++ view/success.php
@@ -1,8 +1,7 @@
-<h1>Đặt hàng thành công</h1>
-<p>Cảm ơn quý khách đã mua hàng</p>
-<button><a href="index.php">Trở về</a></button>
-<script>Swal.fire({
-  title: "Đặt Hàng Thành Công ",
-//   text: "You clicked the button!",
-  icon: "success"
-});</script>
+  <h1>Đặt hàng thành công</h1>
+  <p>Cảm ơn quý khách đã mua hàng</p>
+  <button><a href="index.php">Trở về</a></button>
+  <script>Swal.fire({
+    title: "Đặt Hàng Thành Công ",
+    icon: "success"
+  });</script>
